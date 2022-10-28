b910ed942c701518f3ed28391a176d4f93eda50f
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2022-10-27 23:38:29 -0400

    update
    added the ability for players to move in the scene

---
+++ TurnedBasedStrategy/Assets/scripts/CharacterControler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour
+{
+    public float MoveSpeed;
+    public Vector3 MoveTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveTarget = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, MoveTarget, MoveSpeed * Time.deltaTime);
+    }
+
+    public void MoveToPoint(Vector3 pointtomove)
+    {
+        MoveTarget = pointtomove;
+    }
+}
---
+++ TurnedBasedStrategy/Assets/scripts/MovePoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePoint : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        //FindObjectOfType<CharacterControler>().MoveToPoint(transform.position);
+        GameManager.instance.ActivePlayer.MoveToPoint(transform.position);
+    }
+}
